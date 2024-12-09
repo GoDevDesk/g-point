94468401b2f0bd326c89d99d5881f960b645d0a0
GoDevDesk <lucas.reparacion33@gmail.com>
2024-12-08 23:05:25 -0300

    Estilos creacion album content y boton añadir post al album

--- src/app/components/album-content/album-content.component.ts
+++ src/app/components/album-content/album-content.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-album-content',
   templateUrl: './album-content.component.html',
   styleUrls: ['./album-content.component.scss']
 })
-export class AlbumContentComponent implements OnInit {
+export class AlbumContentComponent  {
+  title = 'Título por defecto';
+  isEditingTitle = false;
+  calculatedInputWidth = 0;
 
-  constructor() { }
+  @ViewChild('titleInput') titleInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('title') titleElement!: ElementRef<HTMLHeadingElement>;
 
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) {}
+
+  hoverAddPhoto = false;
+
+  addPhoto() {
+    alert('¡Lógica para agregar una foto!');
+  }
+  ngAfterViewInit() {
+    this.setInputWidth();
   }
 
+  /**
+   * Activa el modo de edición y enfoca el input dinámicamente.
+   */
+  startEditing() {
+    this.isEditingTitle = true;
+
+    setTimeout(() => {
+      this.setInputWidth();
+      this.titleInput?.nativeElement.focus();
+    });
+  }
+
+  /**
+   * Calcula el tamaño dinámico del campo de texto para coincidir con el tamaño real del título visible.
+   */
+  setInputWidth() {
+    const titleEl = document.querySelector('h2');
+    if (titleEl) {
+      this.calculatedInputWidth = titleEl.offsetWidth;
+    }
+  }
+
+  /**
+   * Guarda el título y sale del modo de edición.
+   */
+  saveTitle() {
+    this.isEditingTitle = false;
+  }
 }
